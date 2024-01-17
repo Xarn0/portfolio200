20af93dda75f842d618ee68c497a73931b2cb90a
xarn0 <xouli@list.ru>
2024-01-17 20:03:41 +0700

    update #2024.1

--- src/components/works-components/works-small.vue
+++ src/components/works-components/works-small.vue
@@ -2,13 +2,10 @@
 include ../../assets/pugmix/mixin.pug
 .conteiner.worksSmall.public_margin
     .worksSmall__header
-        +CreateH("small-projects",false,false)
+        +CreateH("Тестовые задание",false,false)
     .worksSmall__content.public_margin 
-        +CreatSmallbtn("Discord.js","Bot boilerplate","Start creating scalable discord.js bot with typescript in seconds","github")
-        +CreatSmallbtn("Discord.js","Bot boilerplate","Start creating scalable discord.js bot with typescript in seconds","github")
-        +CreatSmallbtn("Discord.js","Bot boilerplate","Start creating scalable discord.js bot with typescript in seconds","github")
-        +CreatSmallbtn("Discord.js","Bot boilerplate","Start creating scalable discord.js bot with typescript in seconds","github")
-        +CreatSmallbtn("Discord.js","Bot boilerplate","Start creating scalable discord.js bot with typescript in seconds","github")
+        +CreatCard("@/static/image/tz1.png","HTML SCSS Pug VUE VUEX VUE-router","Тестовое задание №1 ","Описание: В этом тестовом задание было реализовано следующие, панели связанные между собой, при нажатие на правую панель на любой цвет квадрата он удалиться, при нажатие кнопки сортировать, все квадратики перемешиваются, в левой панели можно добавить квадратики, можно изменить их цвет, можно их отключить(как строчку, так и весь блок с квадратами) ","Открыть <~>",false,"Cached >=", "https://xarn0.github.io/proContent/")
+        +CreatCard("@/static/image/tz2.png","HTML SCSS Pug VUE VUEX VUE-router","Тестовое задание №2 ","Описание: Здесь реализовал подобие мини-игры, сделано вращение барабана, и выводит  комбинации(Их порядка 15-20), сделал подcветку комбинаций, когда у вас закончится монеты игра остановится, так-же можно менять ставку каждого круга 'снизу в левом углу'(Но у вас не получится изменить ставку если она больше количества ваших монет) ","Открыть <~>",false,"Cached >=", "https://xarn0.github.io/royal-mini-game/")  
     
 
 
--- src/views/worksView.vue
+++ src/views/worksView.vue
@@ -3,13 +3,23 @@ include ../assets/pugmix/mixin.pug
 .container
     div.public_margin      
         +CreatBread("Проекты","Список моих проектов")
-    worksAppsBig
-    worksSmall
+    div.btn__actions
+        button(@click="nameComponent='worksSmall'", :class="{'active': (nameComponent=='worksSmall'? true : false )}") Тестовые задание
+        button(@click="nameComponent='worksAppsBig'", :class="{'active': (nameComponent=='worksAppsBig'? true : false )}") Прочие работы
+    Transition(name="animate" , mode="out-in")
+        component(:is="nameComponent")
+    //- worksAppsBig
+    //- worksSmall
 </template>
 <script>
 import worksAppsBig from '@/components/works-components/works-apps-big.vue';
 import worksSmall from '@/components/works-components/works-small.vue';
 export default{
+    data(){
+        return{
+            nameComponent:'worksAppsBig'
+        }
+    },
     components:{
         worksAppsBig,worksSmall
     }
@@ -18,12 +28,39 @@ export default{
 
 
 </script>
-<style scoped>
+<style scoped lang="scss">
 .container{
     text-align: left;
 }
 .breadCrouh{
 
 }
+.animate-enter,.animate-leave-to{
+  opacity: 0;
+}
 
+.animate-enter-active,.animate-leave-active{
+  transition: opacity .7s cubic-bezier(0.25, 0.46, 0.45, 0.54);
+}
+.btn__actions{
+    margin-top: 20px;
+    display: flex;
+    gap: 15px;
+    flex-wrap: wrap;
+    button{
+        padding: 20px;
+        outline: none;
+        opacity: .5;
+        border: 1px solid #ccc;
+        background:rgb(92, 84, 84);
+        border-radius: 15px;
+        font-size: 18px;
+        cursor: pointer;
+    }
+    button.active{
+        color: #fff;
+        opacity: 1;
+        border: 1px solid #C778DD;
+    }
+}
 </style>
